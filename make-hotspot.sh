19100c6ae7057338c6ee7cf2d5808b3ee7eec5ed
A.Amani <didi1364@gmail.com>
2024-12-06 22:52:34 +0330

    fix: add automatic start and end date

--- make-hotspot.sh
+++ make-hotspot.sh
@@ -4,44 +4,44 @@ ls /home -la
 echo " "
 read -p "Enter the folder name: " foldername
 git config --global --add safe.directory /home/"${foldername}"
-end_date=$(cd /home/${foldername} &&  git log --reverse --format=%cd --date=short | head -n 1)
+
+firs_commit_date=$(cd /home/${foldername} &&  git log --reverse --format=%cd --date=short | head -n 1)
 
 # Get the last commit date
 
-start_date=$(cd /home/${foldername} && git log -1 --format=%cd --date=short)
+last_commit_date=$(cd /home/${foldername} && git log -1 --format=%cd --date=short)
 
-read -p "start date(${start_date}): " start_date
+read -p "first commit date($firs_commit_date): " first_commit_date_input
 #current_date=$(date '+%Y-%m-%d')
 
-read -p "end date(${end_date}): " end_date
+read -p "laste_commit_date(${last_commit_date}): " last_commit_date_input
 
-if [ -z "$start_date" ]; then
+if [ -z "$first_commit_date_input" ]; then
 
-	    start_date=$start_date
+	first_commit_date_input=$firs_commit_date
+fi
+
+if [ -z "$last_commit_date_input" ]; then
+
+	    last_commit_date_input=$last_commit_date
 
 fi
 
-if [ -z "$end_date" ]; then
+echo "start from: $first_commit_date_input"
 
-	    end_date=$end_date
-
-fi
-
-echo "Using Start date: $start_date"
-
-echo "Using End date: $end_date"
+echo "to : $last_commit_date_input"
 
 # Validate the date formats before running git log
 
-if ! date -d "$start_date" "+%Y-%m-%d" >/dev/null 2>&1; then
+if ! date -d "$firs_commit_date_input" "+%Y-%m-%d" >/dev/null 2>&1; then
 
-     echo "Start date is invalid: $start_date"
+     echo "Start date is invalid: $first_commit_date_input"
      exit 1
 
 fi
 
-if ! date -d "$end_date" "+%Y-%m-%d" >/dev/null 2>&1; then
-    echo "End date is invalid: $end_date"
+if ! date -d "$last_commit_date_input" "+%Y-%m-%d" >/dev/null 2>&1; then
+    echo "End date is invalid: $last_commit_date_input"
     exit 1
 
 fi
@@ -56,11 +56,11 @@ fi
 #git config --global --add safe.directory /home/"${foldername}"
 mkdir -p /home/result
 cd /home/"${foldername}" && \
-	git log --pretty=format:'[%h] %an %ad %s' --date=short \
-		--numstat --before=$end_date --after=$start_date > /home/result/git_evo.log && \
+	git --no-pager log --pretty=format:'[%h] %an %ad %s' --date=short \
+		--numstat --before=$last_commit_date_input --after=$first_commit_date_input > /home/result/git_evo.log && \
 			cloc ./ --by-file --csv --quiet --report-file=/home/result/maat_lines.csv && \
-				maat -l /home/result/git_evo.log -c git -a revisions > /home/result/maat_freqs.csv
+			java -jar /usr/bin/maat -l /home/result/git_evo.log -c git -a revisions > /home/result/maat_freqs.csv
 	
 cd /home/result && \
-	python3 /app/maat-scripts/transform/csv_as_enclosure_json.py --structure ./maat_lines.csv --weights ./maat_freqs.csv > output.json
+	python3 /maat-scripts/transform/csv_as_enclosure_json.py --structure ./maat_lines.csv --weights ./maat_freqs.csv > output.json
 cp /home/visualization/* -r /home/result 
